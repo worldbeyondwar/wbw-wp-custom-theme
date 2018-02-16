fe32e9728526f0e0340b6a49802b8930d837f26c
marceliotstein <marc.eliot.stein@gmail.com>
2018-02-16 16:11:39 -0500

    changing fonts for header and menu

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
   add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
   function enqueue_parent_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+    $parent_style = 'yamidoo-style'; 
+    wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css',
+                     array($parent_style),
+                     wp_get_theme()->get('Version')
+    );
   }
 
