540d2f5ee6cc1c97ccc081cf7718a827d374679c
marceliotstein <marc.eliot.stein@gmail.com>
2018-04-18 10:13:27 -0400

    added numerical pagination to article archive

--- functions.php
+++ functions.php
@@ -1,11 +1,30 @@
 <?php
-  add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
-  function enqueue_parent_styles() {
-    $parent_style = 'yamidoo-style'; 
-    wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css',
-                     array($parent_style),
-                     wp_get_theme()->get('Version')
-    );
-  }
+/*
+ * functions.php for World Beyond War yamidoo child theme
+ */
 
+add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+
+function enqueue_parent_styles() {
+  $parent_style = 'yamidoo-style'; 
+  wp_enqueue_style($parent_style, get_template_directory_uri().'/style.css');
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css',
+                   array($parent_style),
+                   wp_get_theme()->get('Version')
+  );
+}
+
+if (!function_exists('my_pagination')):
+  function my_pagination() {
+    global $wp_query;
+
+    $big = 999999999; // need an unlikely integer
+
+    echo paginate_links(array(
+      'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+      'format' => '?paged=%#%',
+      'current' => max( 1, get_query_var('paged') ),
+      'total' => $wp_query->max_num_pages
+    ));
+  }
+endif;
--- page-archive.php
+++ page-archive.php
@@ -1,64 +1,62 @@
+<?php
 /**
  * Template Name: WBW Archive
  * 
  * This template generates a custom archive page
  */
+?>
 
 <?php get_header(); ?>
 
 <div class="article-archive">
   <h1 class="page-title">Articles</h1>
  
-         <?php
-           $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : '1';
-           $args = array (
-             'nopaging'               => false,
-             'paged'                  => $paged,
-             'posts_per_page'         => '15',
-             'post_type'              => 'post',
-             'post_status'            => 'publish',
-           );
+  <?php
+    $paged = (get_query_var('paged')) ? get_query_var('paged') : '1';
+    $args = array(
+      'nopaging'               => false,
+      'paged'                  => $paged,
+      'posts_per_page'         => '15',
+      'post_type'              => 'post',
+      'post_status'            => 'publish',
+    );
  
-           $query = new WP_Query( $args );
+    $wp_query = new WP_Query( $args );
  
-           if ($query->have_posts()) {
+    if ($wp_query->have_posts()) {
+      ?>
+      <div class="archive-post">
+        <table class="archive-post-table">
+          <?php
+            while ($wp_query->have_posts()) {
+              $wp_query->the_post();
+              $title = get_the_title();
+              $datetime = get_the_date('F j, Y g:i a');
+              $thumb = get_the_post_thumbnail(null,'medium');
+              $link = get_permalink($recent["ID"]);
+              ?>
+                <tr>
+                  <td class="archive-post-image">
+                    <a href="<?php print $link ?>"><?php print $thumb ?></a>
+                   </td>
+                   <td class="archive-post-text">
+                     <div class="archive-post-title"><h2 class="title"><a href="<?php print $link ?>"><?php print $title ?></a></h2></div>
+                     <div class="archive-post-datetime"><?php print $datetime; ?></div>
+                     <div class="archive-post-summary"><?php the_excerpt(); ?></div>
+                   </td>
+                </tr>
+              <?php
+            }
+          ?>
+        </table>
+      </div>
+      <?php 
+      my_pagination(); 
+    }
  
-             previous_posts_link( '« Previous Page' );
- 
-             ?>
-             <div class="archive-post">
-               <table class="archive-post-table">
-                 <?php
-                   while ($query->have_posts()) {
-                     $query->the_post();
-                     $title = get_the_title();
-                     $datetime = get_the_date('F j, Y g:i a');
-                     $thumb = get_the_post_thumbnail(null,'medium');
-                     $link = get_permalink($recent["ID"]);
-                     ?>
-                     <tr>
-                       <td class="archive-post-image">
-                         <a href="<?php print $link ?>"><?php print $thumb ?></a>
-                       </td>
-                       <td class="archive-post-text">
-                         <div class="archive-post-title"><h2 class="title"><a href="<?php print $link ?>"><?php print $title ?></a></h2></div>
-                         <div class="archive-post-datetime"><?php print $datetime; ?></div>
-                         <div class="archive-post-summary"><?php the_excerpt(); ?></div>
-                       </td>
-                     </tr>
-                     <?php
-                   }
-                 ?>
-               </table>
-             </div>
-             <?php
- 
-             next_posts_link( 'Next Page »', $query->max_num_pages );
-         }
- 
-         // Restore original Post Data
-         wp_reset_postdata();
-         ?>
+    // Restore original Post Data
+    wp_reset_postdata();
+  ?>
 </div>
 
 <?php get_footer(); ?>
